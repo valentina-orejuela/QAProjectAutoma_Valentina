5542370a7a046f38521dd349e5bda53e75b49849
Valentina <57046128+valentina-orejuela@users.noreply.github.com>
2022-08-22 00:18:48 -0500

    Create a pestore
    Create a pestore project and automate the test cases in
    Karate.

---
+++ pet_store_karate/petstore/src/test/java/karate/request/deletePet.java
@@ -0,0 +1,11 @@
+package karate.request;
+
+import com.intuit.karate.junit5.Karate;
+
+public class deletePet {
+    @Karate.Test
+    Karate testSample(){
+        return Karate.run("classpath:karate/request/deletePet.feature").relativeTo(getClass());
+    }
+
+}
