7adf44198c74a5ff2c875758b46895749b28ed07
valentina <valentina.orejuela.a@gmail.com>
2022-08-22 00:09:45 -0500

    Create a pestore project and automate the test cases in karate

--- src/test/java/karate/request/addPets.java
+++ src/test/java/karate/request/addPets.java
@@ -2,7 +2,7 @@ package karate.request;
 
 import com.intuit.karate.junit5.Karate;
 
-public class addPets {
+public class addPets{
     @Karate.Test
     Karate testSample(){
         return Karate.run("classpath:karate/request/addPets.feature").relativeTo(getClass());
