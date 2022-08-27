f840e0d1773991e521567fae87bae18858369282
valentina-orejuela <valentina.orejuela.a@gmail.com>
2022-08-27 13:18:28 -0500

    Update project

--- pet_store_karate/src/test/java/karate/request/updatePet/UpdatePet.java
+++ pet_store_karate/src/test/java/karate/request/updatePet/UpdatePet.java
@@ -5,7 +5,7 @@ import com.intuit.karate.junit5.Karate;
 public class UpdatePet {
     @Karate.Test
     Karate testSample(){
-         return Karate.run("classpath:karate/request/updatePet/UpdatePet.feature").relativeTo(getClass());
+        return Karate.run("classpath:karate/request/updatePet/UpdatePet.feature").relativeTo(getClass());
     }
 
 }
